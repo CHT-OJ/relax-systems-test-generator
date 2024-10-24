e24410e710d5b1ae23b3c0f23f1bbe43e152ebd9
ntannn <nhattanff07@gmail.com>
2024-10-24 16:44:27 +0700

    ọc ọc ọc

--- testgenerator.cpp
+++ testgenerator.cpp
@@ -1,4 +1,6 @@
 #include<template.h>
+#include<testlib.h>
+
 #pragma GCC optimize("O3,unroll-loops,no-stack-protector")
 #pragma GCC target("sse4,avx2,fma")
 
@@ -9,14 +11,14 @@ using namespace std;
 
 typedef long long ll;
 
-const int btest = 1; 
-const int etest = 50; // so luong test
+const int begintest = 1; 
+const int endtest = 50; // so luong test
 const int test_for_sub1 = 20; //% so luong test theo tung sub
 const int test_for_sub2 = 80;
 const int test_for_sub3 = 100;
-const int s1 = double(etest) / 100.0 * test_for_sub1 * 1.0;
-const int s2 = double(etest) / 100.0 * test_for_sub2 * 1.0;
-const int s3 = etest - s2;
+const int s1 = double(endtest) / 100.0 * test_for_sub1 * 1.0;
+const int s2 = double(endtest) / 100.0 * test_for_sub2 * 1.0;
+const int s3 = endtest - s2;
 
 
 
@@ -43,7 +45,7 @@ void sub3(int x) {
 int main() {
     srand(chrono::steady_clock::now().time_since_epoch().count());
 
-    for (int i = btest; i <= etest; i++)
+    for (int i = begintest; i <= endtest; i++)
     {
         if(i <= s1)
             sub1(i);
